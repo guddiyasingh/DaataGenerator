6afb2762d437c214ebf0c7aff67bde3f2242497f
Guddiya Singh <guddiyasingh1847@gmail.com>
2025-07-12 19:36:27 +0530

    add some code

---
+++ main.js
@@ -0,0 +1,13 @@
+// Generate a dummy data in this format in a collection called Employees in db called company
+
+// {
+//     name: "Shivam",
+//     salary: "40000000",
+//     lanaguage: "English",
+//     country:"India",
+//     isManager: true
+// }
+
+// Gererate 10 such records when a button called generate data is clicked!
+// Create an Express app with mongoose to acheive it Everytime the button is clicked , 
+// you should collection the table
