0ae19f74feb14c28e73f722afd59bde4cc19fbb8
Guddiya Singh <guddiyasingh1847@gmail.com>
2025-07-12 21:09:40 +0530

    add some code in Employee file

---
+++ models/Employee.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const EmployeeSchema = new mongoose.Schema({
+ 
+name: {type: String,  required: true},
+salary: {type: Number, required: true},
+language: {type: String, required: true},
+country: {type: String, required: true},
+isManager: {type: Boolean, default: false}
+})
