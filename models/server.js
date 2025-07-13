ca28f7677b2d8f8894951a26c1226703a1bf4762
Guddiya Singh <guddiyasingh1847@gmail.com>
2025-07-13 16:50:39 +0530

    add some code in server

--- models/server.js
+++ models/server.js
@@ -5,3 +5,16 @@ const path = require('path');
 
 const app = express();
 const PORT = 3000;
+
+mongoose.connect("mongodb://localhost:27017/todo")
+.then(()=>console.log('MongoDB connected'));
+
+// --------Dummy-data generator---------
+
+const firstNames = [
+
+    'Shivam','Aarav','Isha',"priya",'Rahul','Sneha', "Vikram",'Kiran','Dev', "Neha" ];
+
+    function buildDummyEmployee(i) {
+        
+    } 
